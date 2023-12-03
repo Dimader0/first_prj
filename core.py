de14ac690fd760955f890a20399495bc8e171206
Gotliha <kostenkonikita32@gmail.com>
2023-12-03 17:14:29 +0200

    secondcommit8

--- core.py
+++ core.py
@@ -1,17 +1,43 @@
 from main import*
 import json
 
+notes = dict()
+
+try:
+    with open("data.json", "r", encoding= "utf-8") as file:
+        notes = json.load(file)
+    listnotes.addItems(notes)
+except:
+    print("Такого файлу не існує!")
+
 def show_note():
-    pass
+    textEdit.clear()
+    key = listnotes.selectedItems()[0].text()
+    textEdit.setText(notes[key]["текст"])
 
 def add_note():
     note_name, ok = QInputDialog.getText(mv, "Додати замітку", "Назва замітки")
+    if ok and note_name != "":
+        notes[note_name] = {"текст": "", "теги": []}
+        listnotes.addItem(note_name)
+        listtags.addItems(notes[note_name]["теги"])
+        print(notes)
 
 def save_note():
-    pass
+    if listnotes.selectedItems():
+        key = listnotes.selectedItems()[0].text()
+        notes[key]["текст"] = textEdit.toPlainText()
+        with open("data.json", "w", encoding= "utf-8") as file:
+            json.dump(notes, file, sort_keys= True, ensure_ascii= False)
+    else:
+        print("Ви не вибрали замітку!")
 
 def del_note():
     pass
+
+btn_createnote.clicked.connect(add_note)
+btn_savenote.clicked.connect(save_note)
+listnotes.itemClicked.connect(show_note)
 
 mv.show()
 app.exec_()
